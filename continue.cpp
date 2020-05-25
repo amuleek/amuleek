b0fe4c754f961de6812d6a2990b874d16ecc957e
Amuleek Sidhu <46739438+amuleek@users.noreply.github.com>
2020-05-25 19:22:12 +0530

    Create continue.cpp

---
+++ continue.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 10; ++i)
+    {
+        if ( i == 6 || i == 9)
+        {
+            continue;
+        }
+        cout << i << "\t";
+    }
+    return 0;
+}
